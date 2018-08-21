aa1c73af67e9e82256c31c7cf9989a348dd6cd6e
lakhin9818 <lakhingupta@gmail.com>
2018-08-21 12:31:52 +0530

    Web Form

---
+++ simpleform/simplewebform.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="simplewebform.aspx.cs" Inherits="simpleform.simplewebform" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>name</td>
+                    <td><asp:TextBox id="name" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Age</td>
+                    <td><asp:TextBox ID="age" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Adress</td>
+                    <td><asp:TextBox ID="address" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><asp:Button Text="save" ID="save" runat="server" OnClick="save_Click"/>
+                        <asp:Button Text="reset" ID="reset" runat="server" OnClick="reset_Click"/></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><asp:GridView ID="grd" runat="server" OnRowCommand="grd_RowCommand" AutoGenerateColumns="false" >
+                        <Columns>
+                            <asp:TemplateField HeaderText="name" >
+                                <ItemTemplate>
+                                    <%#Eval("name") %>
+                                </ItemTemplate>
+
+                            </asp:TemplateField>
+                             <asp:TemplateField HeaderText="age" >
+                                <ItemTemplate>
+                                    <%#Eval("age") %>
+                                </ItemTemplate>
+
+                            </asp:TemplateField> <asp:TemplateField HeaderText="address" >
+                                <ItemTemplate>
+                                    <%#Eval("address") %>
+                                </ItemTemplate>
+
+                            </asp:TemplateField>
+                             <asp:TemplateField HeaderText="delete" >
+                                <ItemTemplate>
+                                 <asp:LinkButton id="delete" runat="server" Text="delete" CommandName="DLT" CommandArgument='<%#Eval("id") %>' ></asp:LinkButton>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            
+                             <asp:TemplateField HeaderText="edit" >
+                                <ItemTemplate>
+                                 <asp:LinkButton id="edit" runat="server" Text="edit" CommandName="EDT" CommandArgument='<%#Eval("id") %>' ></asp:LinkButton>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                        </asp:GridView></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ simpleform/simplewebform.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace simpleform
+{
+    public partial class simplewebform : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["cnstr"].ConnectionString);
+            
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Display(); 
+        }
+        public void Display()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("show", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            cmd.CommandType = CommandType.StoredProcedure;
+            da.Fill(ds);
+            con.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                grd.DataSource = ds;
+                grd.DataBind();
+            }
+            else
+            {
+                grd.DataSource = null;
+                grd.DataBind();
+            }
+        }
+
+        protected void save_Click(object sender, EventArgs e)
+        {
+            if (save.Text == "save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insrt", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@name", name.Text);
+                cmd.Parameters.AddWithValue("@age", age.Text);
+                cmd.Parameters.AddWithValue("@address", address.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Display();
+            }
+            else if (save.Text == "update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPT", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id",ViewState["upt"]);
+                cmd.Parameters.AddWithValue("@name", name.Text);
+                cmd.Parameters.AddWithValue("@age", age.Text);
+                cmd.Parameters.AddWithValue("@address", address.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Display();
+
+            }
+        }
+        public void Clear()
+        {
+            name.Text = "";
+            address.Text = "";
+            age.Text = "";
+        }
+
+        protected void reset_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "DLT") {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DLT", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Display();
+            }
+            else if (e.CommandName=="EDT")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("EDT", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    name.Text = ds.Tables[0].Rows[0]["name"].ToString();
+                    age.Text = ds.Tables[0].Rows[0]["age"].ToString();
+                    address.Text = ds.Tables[0].Rows[0]["address"].ToString();
+                    save.Text = "update";
+                    ViewState["upt"] = e.CommandArgument;
+                }
+            }
+        }
+    }
+}
